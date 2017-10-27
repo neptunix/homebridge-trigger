88beba5fc3e1658bd1da8f7c32aa5f62446888b6
Dmitry Brigadirov <brigadirov@itsph.ru>
2017-10-27 13:25:19 +0300

    Added FakeTrigger code

--- README.md
+++ README.md
@@ -13,9 +13,20 @@ It lookes quite trashy and inreliable - if the temperature value does not change
 # How?
 This is more like a hack to overcome the limitation of HomeKit not able to trigger actions on custom values. It works like that:
 * Add a fake contact sensor that automatically triggers every n-seconds
-* Make a rule "Turn the lights off when FakeSensor changes state and power consumption exceeds 100w"
+* Make a rule "Turn the lights off when FakeTrigger changes state and power consumption exceeds 100w"
 
 Lets set n to 60 seconds and HomeKit will run your rule every minute.
 Now the cons. Need to check the impact on the iPhone's battery as it's the place where HomeKit lives (if you don't have an Apple TV).
 Even with Apple TV not sure if it doesn't make you phone process the trigger every single time (hope it doesn't)
 
+# Configuration
+Install homebridge-trigger plugin and add accessories to your config.json `accessories` section. Example:
+```
+"accessories": [        
+        {
+          "accessory": "FakeTrigger",
+          "name": "Trigger every 30 sec",
+          "interval": 30
+        }
+    ]
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let Service, Characteristic, HomebridgeAPI, UUIDGen
+
+module.exports = (homebridge) => {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  HomebridgeAPI = homebridge
+  UUIDGen = homebridge.hap.uuid
+
+  homebridge.registerAccessory("homebridge-trigger", "FakeTrigger", FakeTrigger);
+}
+
+class FakeTrigger {
+  constructor(log, config) {
+    this.log = log;
+    this.name = config.name;
+    this.interval = config.interval * 1000;
+  
+    this._sensor = new Service.ContactSensor(this.name)
+    this._sensor.contactState = false
+    this._sensor.getCharacteristic(Characteristic.ContactSensorState)
+      .on('get', (callback) => {
+        //this.log(`Gettings state ${this._sensor.contactState}`)
+        callback(null, this._sensor.contactState)
+      })
+    
+
+    setInterval( () => {
+      // Change sensor state every this.interval seconds
+      this._sensor.contactState = !this._sensor.contactState
+      //this.log(`Setting state to ${this._sensor.contactState}`)
+      this._sensor.setCharacteristic(Characteristic.ContactSensorState, this._sensor.contactState)
+    }, this.interval)
+  }
+
+  getServices () {
+    return [this._sensor]
+  }
+}
